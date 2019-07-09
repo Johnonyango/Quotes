5ab42b81168b2ca4ed5b0da8700b83c4695041a6
Johnonyango <j.yayah7@gmail.com>
2019-07-09 12:50:41 +0300

    modified quote-detail,quote.component and quote-form component files

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -4,7 +4,7 @@ export class Quote {
       public id: number,
       public description: string,
       public name: string,
-      public publisher: string,
+      public submitter: string,
       public initialVote: number,
       public finalVote: number,
       public completeDate: Date) {
